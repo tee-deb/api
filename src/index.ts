4384cfdee5780e8b1df59471a53ca1954ee3cc6e
tee-deb <solodey4uu@gmail.comm>
2024-12-13 19:16:12 -0800

    'Setting up api and express'

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import productsRoutes from './routes/products/index';
+
+
+const port = 3000;
+
+
+const app = express();
+
+
+app.get('/',  (req, res) => {
+    res.send('Hello World!');
+  });
+
+
+
+  app.use('/products', productsRoutes);
+
+  app.listen(port, () => {
+    console.log(`Example app is running on port ${port}`)
+  });
+
